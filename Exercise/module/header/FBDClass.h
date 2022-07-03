1a5144bf4f0422ce8a6c6d7ab45e3c26aa19a941
giang-nguyentbk <giang.nguyentbk@gmail.com>
2022-07-03 12:08:45 +0700

    <Add> [Exercise/module/] FBDClass

---
+++ Exercise/module/header/FBDClass.h
@@ -0,0 +1,22 @@
+#ifndef _FBD_CLASS_H_
+#define _FBD_CLASS_H_
+
+#include <vector>
+#include "FBClass.h"
+
+using namespace std;
+
+class FBD : public vector<FB*> {
+    private:
+        double Ts;
+        bool running;
+    public:
+        FBD(double ts=0.5);
+        ~FBD();
+        void addFB(FB* p);
+        void connect(int fb1, int fb2, int fb1_outport=0, int fb2_inport=0);
+        void start();
+        void stop();
+};
+
+#endif // _FBD_CLASS_H_
---
+++ Exercise/module/src/FBDClass.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include "../header/FBDClass.h"
+
+using namespace std;
+
+FBD::FBD(double ts) : Ts(ts>0 ? ts : 1), running(true) {}
+
+FBD::~FBD() {
+    for(int i=0; i<size(); i++) {
+        delete at(i);
+    }
+}
+
+void FBD::addFB(FB* p) {
+    push_back(p);
+}
+
+void FBD::connect(int fb1, int fb2, int fb1_outport, int fb2_inport) {
+    FB* pFB1 = at(fb1);
+    FB* pFB2 = at(fb2);
+    pFB2->setInputPort(pFB1->getOutputPort(fb1_outport), fb2_inport);
+}
+
+void FBD::start() {
+    while(running) {
+        for(int i=0; i<size(); i++) {
+            at(i)->execute();
+        }
+    }
+}
+
+void FBD::stop() {
+    running = false;
+}
